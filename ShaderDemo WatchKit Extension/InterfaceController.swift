25d8bdfe44ccdcd89430b4c97e2b63ed994b36c5
Allan Lavell <alavell@gmail.com>
2016-06-25 11:50:20 -0300

    Initial Commit

---
+++ ShaderDemo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  ShaderDemo WatchKit Extension
+//
+//  Created by Allan Lavell on 2016-06-25.
+//  Copyright © 2016 ThinkRad Studios Inc. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: AnyObject?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
